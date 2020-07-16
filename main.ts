5a93c4500a403b68c63ece24547e295fa6b91f34
Fanny <67992428+FannyVanbuckhave@users.noreply.github.com>
2020-07-16 17:54:12 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     led.unplot(x, y)
     x += -1
+    if (x == -1) {
+        y += 1
+        x = 4
+    }
+    if (x == 0 && y == 0) {
+        x = 4
+        y = 4
+    }
 })
 input.onButtonPressed(Button.B, function () {
     led.unplot(x, y)
-    x += -1
-    if (x < 0) {
+    x += 1
+    if (x == 5) {
+        y += 1
         x = 0
+    }
+    if (x == 4 && y == 4) {
+        x = 0
+        y = 0
     }
 })
 let y = 0
